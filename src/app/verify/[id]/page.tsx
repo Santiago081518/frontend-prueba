1b850033478e3991b518b248d4ead8b3f7dac8ca
Santiago Botero <sboterovalencia@gmail.com>
2026-05-13 23:18:00 -0500

    Se arregla baseUrl

--- src/app/verify/[id]/page.tsx
+++ src/app/verify/[id]/page.tsx
@@ -17,7 +17,7 @@ export default function VerifyPrescription({ params }: { params: Promise<{ id: s
     if (!id) return;
 
     // Obtén la URL base de tu variable de entorno
-    const baseURL = 'https://tu-api-en-render.onrender.com';
+    const baseURL = 'https://backend-prueba-0g6r.onrender.com';
 
     // USA AXIOS DIRECTO AQUÍ (sin interceptores)
     axios.get(`${baseURL}/prescriptions/public/verify/${id}`)
